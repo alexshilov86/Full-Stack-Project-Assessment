2d25926594f112f21d2a798bd4e205be117091a1
alexshilov86 <avshilov995@gmail.com>
2022-07-18 11:12:59 +0300

    level 100
    add form to add video

--- client/src/container.js
+++ client/src/container.js
@@ -33,16 +33,53 @@ function Like(props){
         </div>
     )
 }
-
+function Addform(props){
+    return (
+        <div id="addform" className="addform">
+            <div className="title-video">
+                <span>Title: </span>
+                <input id="title-input" placeholder="Please input Title..."></input>
+            </div>
+            <div className="title-video">
+                <span>URL: </span>
+                <input id="url-input" placeholder="Please input URL..."></input>
+            </div>
+            <div>
+                <button className="cancel-button" onClick={()=>{
+                    let addformel = document.getElementById("addform");
+                    addformel.style.display = "none";
+                }}>Cancel</button>
+                <button className="cancel-button" onClick={()=>{
+                    let title = document.getElementById("title-input");
+                    let url = document.getElementById("url-input");
+                    props.addvideofunc(title.value, url.value);
+                }}>Add</button>
+            </div>
+        </div>
+    )
+}
 
 function Container(props){
     const [videolist, setVideolist] = useState(props.data)
     function deleteVideo(id) {
         setVideolist(videolist.filter(e => e.id !== id))
     }
+    function addVideo(title, url) {
+        let newvideo = {"id" : 1, "title" : title, "url" : url, "rating" : 20};
+        setVideolist(props.data.concat(newvideo));
+    }
     return (
-        <div className="container">
-            {videolist.map(e=> <VideoBox data={e} funcdelete={deleteVideo}/>)}
+        <div>
+            <div className="sear-add-chbox">
+                <button onClick={()=>{
+                    let addformel = document.getElementById("addform");
+                    addformel.style.display = "block";
+                }}>Add</button>
+                <Addform addvideofunc={addVideo}/>
+            </div>
+            <div className="container">
+                {videolist.map(e=> <VideoBox key={e.id} data={e} funcdelete={deleteVideo}/>)}
+            </div>
         </div>
     )
 }
